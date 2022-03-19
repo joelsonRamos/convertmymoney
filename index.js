60ab782a962460acd0cda60df4118837b9e29574
joelsonRamos <joelson.engcomp@gmail.com>
2022-03-19 20:53:43 -0300

    versel json

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 const express = require('express')
 const app = express()
+//tratamento do path
 const path = require('path')
 
 const convert = require('./lib/convert')
 const apiBCB = require('./lib/api.bcb')
-
+//necessario setar o view engine
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views')) // aqui eu passo qual o diretorio 
-// vão ficar minha pages
-/////////////////////////
+
+app.set('views',path.join(__dirname, 'views'))
+
+//local para por meus arquivos como css, html
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', async(req, res) => {
+app.get('/', async(req, res)=>{
     const cotacao = await apiBCB.getCotacao()
     res.render('home', {
         cotacao
     })
+    
 })
-app.get('/cotacao', (req, res) =>{
+
+app.get('/cotacao',(req, res)=>{
     const {cotacao, quantidade} = req.query
-    if(cotacao && quantidade) {
+   
+    if(cotacao && quantidade){
         const conversao = convert.convert(cotacao, quantidade)
         res.render('cotacao', {
             error: false,
-            cotacao: convert.toMoney(cotacao),
-            quantidade: convert.toMoney(quantidade),
-            conversao: convert.toMoney(conversao)
+            cotacao: convert.toManey(cotacao),
+            quantidade: convert.toManey(quantidade),
+            conversao: convert.toManey(conversao)
         })
-    }else {
-        res.render('cotacao', {
+
+    }else{
+        res.render('cotacao',{
             error: 'Valores inválidos'
         })
     }
+    
 })
 
-app.listen(3000, err =>{
-    if (err){
-        console.log('unable to start the server')
+app.listen(3000, err=>{
+    if(err){
+        console.log('Não foi possivel iniciar a aplicação')
     }else{
-
-        console.log('ConvertMyMoney is online')
+        console.log('ConvertMyMoney esta online')
     }
+
 })
--- teste-api.js
+++ teste-api.js
@@ -1,7 +1,6 @@
 const axios = require('axios')
 
-const url = `https://olinda.bcb.gov.br/olinda/servico/PTAX/vers...${data}%27&$top=100&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao`
-
+const url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2701-28-2022%27&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao'
 axios
 .get(url)
 .then(res => console.log(res.data.value[0].cotacaoVenda))
